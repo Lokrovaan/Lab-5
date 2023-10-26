849331fc80263812e0fc85d9f10a53e9c2221e4b
Ubuntu <ubuntu@ip-172-31-33-98.ec2.internal>
2023-10-26 11:26:53 +0000

    Another commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@
 class HelloWorld 
 { 
     // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
+    // Prints "Hello, World" to the terminal window.
+	//Hello 
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
